41fad98bc392343d238f71b6654990552c91cf8a
draaaaaaaaaaaaaaa <moh.daffa.ucui@gmail.com>
2022-04-05 10:48:36 +0700

    comit

--- middleware/index.js
+++ middleware/index.js
@@ -9,8 +9,16 @@ const middlewareBody = require("./middlewares/body")
 
 app.use('/', bodyParser.json(), middlewareHeader);
 
-app.post("/", bodyParser.urlencoded({extended:true}), middlewareBody);
+app.post("/", bodyParser.urlencoded({extended:true}), middlewareBody,(req,res) =>{
+    res.send(req.body);
+});
+
+
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);               
+    PageTransitionEvent.prototype.initPageTransitionEvent = function(typeArg, canBubbleArg, cancelableArg, persistedArg) {
+        PushSubscriptionOptions.prototype.initPushSubscriptionOptions = function(userVisibleOnlyArg, applicationServerKeyArg) {
+            
+
 })
